5552b49e2fc02993c883e45d45189178ad814782
Vasilyg10 <zvgianna@gmail.com>
2021-07-27 09:13:29 -0500

    added more requires to server

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const path = require('path');
+const routes = require('./controllers');
+const { ppid } = require('process');
+const exbhbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'This is a secret',
+    cookie: {},
+    resave: false,
+    saveUninitiated: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+app.use(routes);
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`You are now running on PORT ${PORT}`));
 });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
